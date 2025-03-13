738db451a5a1623acfdec38ad8f4d5355e4c3a0d
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-03-13 16:02:42 +0200

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Image extends Model
 {
-    //
+    public function game(): BelongsTo
+    {
+        return $this->belongsTo(Game::class);
+    }
 }
