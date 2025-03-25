73b6acb6ea12aa08c34d543e3bfbb5c7eb053af9
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-03-25 15:18:36 +0200

    Update Image.php

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Image extends Model
 {
+protected $fillable = [
+    'images',
+    'game_id'
+];
     public function game(): BelongsTo
     {
         return $this->belongsTo(Game::class);
