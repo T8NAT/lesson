26bd99429636e72c681a6a39b1d96c1db50d3913
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-02 17:50:58 +0300

    Update Word.php

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -4,20 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Word extends Model
 {
     protected $fillable = [
-        'words',
+        'word',
         'category_id',
+        'image_id',
     ];
 
-    protected $casts = [
-        'words' => 'array',
-    ];
+//    protected $casts = [
+//        'word' => 'array',
+//    ];
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
 
+    }
+    public function word(): BelongsTo{
+        return $this->belongsTo(Word::class);
     }
 }
