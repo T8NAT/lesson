2e0c1e9ce854689b4c75f2516989854e7b3b3fbc
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-02 17:51:21 +0300

    Create edit.blade.php

---
+++ resources/views/cms/image/edit.blade.php
@@ -0,0 +1,152 @@
+@extends('cms.layout.master')
+@section('toolbar-title','تعديل الصورة')
+@section('breadcrumb','كافة الصور')
+@section('sub-breadcrumb','تعديل الصورة')
+@section('content')
+    <!--begin::Heading-->
+    <div class="card-border mb-13 text-center">
+        <!--begin::Title-->
+        <h1 class="mb-3">تعديل الصورة</h1>
+        <!--end::Title-->
+        <!--begin::Description-->
+        <div class="text-gray-400 fw-bold fs-5">يمكنك تصفح قائمة الصور من
+            <a href="{{route('images.index')}}" class="fw-bolder link-primary">هنا</a>.
+        </div>
+        <!--end::Description-->
+    </div>
+    <!--end::Heading-->
+    <!--begin::Content-->
+    <div id="kt_app_content" class="app-content flex-column-fluid">
+        <!--begin::Content container-->
+        <div id="kt_app_content_container" class="app-container container-xxl">
+            <form id="kt_add_image_form" action="{{ route('images.update',$image->id) }}" enctype="multipart/form-data" method="POST" class="form d-flex flex-column flex-lg-row" data-kt-redirect="{{route('images.create')}}">
+                @csrf
+                @method('PUT')
+                <!--begin::Aside column-->
+                <div class="d-flex flex-column gap-7 gap-lg-10 w-100 w-lg-300px mb-7 me-lg-10">
+                    <!--begin::Thumbnail settings-->
+                    <div class="card card-flush py-4">
+                        <!--begin::Card header-->
+                        <div class="card-header">
+                            <!--begin::Card title-->
+                            <div class="card-title">
+                                <h2>{{'صورة'}}</h2>
+                            </div>
+                            <!--end::Card title-->
+                        </div>
+                        <!--end::Card header-->
+                        <!--begin::Card body-->
+                        <div class="card-body text-center pt-0">
+                            <!--begin::Image input-->
+                            <!--begin::Image input placeholder-->
+                            <style>.image-input-placeholder { background-image: url('{{asset('assets/media/svg/files/blank-image.svg')}}'); } [data-bs-theme="dark"] .image-input-placeholder { background-image: url('assets/media/svg/files/blank-image-dark.svg'); }</style>
+                            <!--end::Image input placeholder-->
+                            <!--begin::Image input-->
+                            <div class="image-input image-input-empty image-input-outline image-input-placeholder mb-3" data-kt-image-input="true">
+                                <!--begin::Preview existing avatar-->
+                                <div class="image-input-wrapper w-150px h-150px" style="background-image: url({{Storage::url($image->image)}})"></div>
+                                <!--end::Preview existing avatar-->
+                                <!--begin::Label-->
+                                <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+                                    <!--begin::Icon-->
+                                    <i class="ki-duotone ki-pencil fs-7">
+                                        <span class="path1"></span>
+                                        <span class="path2"></span>
+                                    </i>
+                                    <!--end::Icon-->
+                                    <!--begin::Inputs-->
+                                    <input type="file" name="image" accept=".png, .jpg, .jpeg" />
+                                    <input type="hidden" name="avatar_remove" />
+                                    <!--end::Inputs-->
+                                </label>
+                                <!--end::Label-->
+                                <!--begin::Cancel-->
+                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel avatar">
+															<i class="ki-duotone ki-cross fs-2">
+																<span class="path1"></span>
+																<span class="path2"></span>
+															</i>
+														</span>
+                                <!--end::Cancel-->
+                                <!--begin::Remove-->
+                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
+															<i class="ki-duotone ki-cross fs-2">
+																<span class="path1"></span>
+																<span class="path2"></span>
+															</i>
+														</span>
+                                <!--end::Remove-->
+                            </div>
+                            <!--end::Image input-->
+                            <!--begin::Description-->
+                            <div class="text-muted fs-7">{{'قم بتعيين صورة. يتم قبول ملفات الصور *.png و*.jpg و*.jpeg فقط'}}</div>
+                            <!--end::Description-->
+                        </div>
+                        <!--end::Card body-->
+                        @error('image')
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+                    <!--end::Thumbnail settings-->
+                </div>
+                <!--end::Aside column-->
+                <!--begin::Main column-->
+                <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
+                    <!--begin::General options-->
+                    <div class="card card-flush py-4">
+                        <!--begin::Card header-->
+                        <div class="card-header">
+                            <div class="card-title">
+                                <h2>{{'عام'}}</h2>
+                            </div>
+                        </div>
+                        <!--end::Card header-->
+                        <!--begin::Card body-->
+                        <div class="card-body pt-0">
+                            <!--begin::Input group-->
+                            <div class="mb-10 fv-row">
+                                <!--begin::Label-->
+                                <label class="required form-label">{{'اسم الصورة'}}</label>
+                                <!--end::Label-->
+                                <!--begin::Input-->
+                                <input type="text" name="name" id="name"  class="form-control mb-2" placeholder="{{'قم بادخال اسم الصورة هنا'}}" value="{{old('name',$image->name)}}" />
+                                <!--end::Input-->
+                                <!--begin::Description-->
+                                <div class="text-muted fs-7">{{'يجب أن يكون اسم الصورة فريدًا ومن المستحسن أن يكون فريدًا.'}}</div>
+                                <!--end::Description-->
+                                @error('name')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                            <!--end::Input group-->
+
+                        </div>
+                        <!--end::Card header-->
+                    </div>
+                    <!--end::General options-->
+                    <div class="d-flex justify-content-end">
+                        <!--begin::Button-->
+                        <a href="{{ route('images.index') }}" id="kt_add_image_cancel" class="btn btn-light me-5">{{'الغاء'}}</a>
+                        <!--end::Button-->
+                        <!--begin::Button-->
+                        <button type="submit"  class="btn btn-primary" id="kt_add_image_submit">
+                            <span class="indicator-label">{{'حفظ التعديلات'}}</span>
+                            <span class="indicator-progress">{{'الرجاء الانتظار'}}...
+                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                        </button>
+                        <!--end::Button-->
+                    </div>
+                </div>
+                <!--end::Main column-->
+            </form>
+        </div>
+        <!--end::Content container-->
+    </div>
+    <!--end::Content-->
+@endsection
+@section('scripts')
+    <script src="{{asset('assets/plugins/custom/formrepeater/formrepeater.bundle.js')}}"></script>
+    <script src="{{asset('assets/js/cms/images/save-image.js')}}"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/i18n/ar.min.js"></script>
+@endsection
