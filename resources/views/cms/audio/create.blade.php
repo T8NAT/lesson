ddb3223b72199b80e019006855c422297b445eaa
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-09 12:17:41 +0300

    Create lessons-453312-572888220f8d.json

---
+++ resources/views/cms/audio/create.blade.php
@@ -0,0 +1,113 @@
+@extends('cms.layout.master')
+@section('toolbar-title','اضافة صوت جديد')
+@section('breadcrumb','كافة الاصوات')
+@section('sub-breadcrumb','اضافة صوت جديد')
+    @section('content')
+        <!--begin::Heading-->
+        <div class="card-border mb-13 text-center">
+            <!--begin::Title-->
+            <h1 class="mb-3">اضافة قسم جديد</h1>
+            <!--end::Title-->
+            <!--begin::Description-->
+            <div class="text-gray-400 fw-bold fs-5">يمكنك تصفح قائمة الاصوات من
+                <a href="{{route('audios.index')}}" class="fw-bolder link-primary">هنا</a>.
+            </div>
+            <!--end::Description-->
+        </div>
+        <!--end::Heading-->
+        <!--begin::Content-->
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <!--begin::Content container-->
+            <div id="kt_app_content_container" class="app-container container-xxl">
+                <form id="kt_add_audio_form" action="{{ route('audios.store') }}" enctype="multipart/form-data" method="POST" class="form d-flex flex-column flex-lg-row" data-kt-redirect="{{route('audios.create')}}">
+                 @csrf
+                    <!--begin::Main column-->
+                    <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
+                        <!--begin::General options-->
+                        <div class="card card-flush py-4">
+                            <!--begin::Card header-->
+                            <div class="card-header">
+                                <div class="card-title">
+                                    <h2>{{'عام'}}</h2>
+                                </div>
+                            </div>
+                            <!--end::Card header-->
+                            <!--begin::Card body-->
+                            <div class="card-body pt-0">
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required form-label">{{'اسم الصوت'}}</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <input type="text" name="name" id="name"  class="form-control mb-2" placeholder="{{'قم بادخال اسم الصوت هنا'}}" value="{{old('name')}}" />
+                                    <!--end::Input-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">{{'يجب أن يكون اسم الفئة الصوت ومن المستحسن أن يكون فريدًا.'}}</div>
+                                    <!--end::Description-->
+                                    @error('name')
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="form-label">{{'الوصف'}}</label>
+                                    <!--end::Label-->
+                                    <!--begin::Editor-->
+                                    <textarea name="description" class="form-control @error('description') is-invalid @enderror">{{old('description')}}</textarea>
+                                    <!--end::Editor-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">{{'قم بتعيين وصف للقسم لتحسين الرؤية.'}}</div>
+                                    <!--end::Description-->
+                                    @error('description')
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="mb-10 fv-row">
+                                    <!--begin::Label-->
+                                    <label class="required form-label">{{'ملف الصوت'}}</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <input type="file" name="path" id="path"   class="form-control mb-2" />
+                                    <!--end::Input-->
+                                    @error('path')
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+                                <!--end::Input group-->
+                            </div>
+                            <!--end::Card header-->
+                        </div>
+
+                        <!--end::General options-->
+                        <div class="d-flex justify-content-end">
+                            <!--begin::Button-->
+                            <a href="{{ route('audios.index') }}" id="kt_add_audio_cancel" class="btn btn-light me-5">{{'الغاء'}}</a>
+                            <!--end::Button-->
+                            <!--begin::Button-->
+                            <button type="submit"  class="btn btn-primary" id="kt_add_audio_submit">
+                                <span class="indicator-label">{{'انشاء'}}</span>
+                                <span class="indicator-progress">{{'الرجاء الانتظار'}}...
+													<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                            </button>
+                            <!--end::Button-->
+                        </div>
+                    </div>
+                    <!--end::Main column-->
+                </form>
+            </div>
+            <!--end::Content container-->
+        </div>
+        <!--end::Content-->
+    @endsection
+@section('scripts')
+    <script src="{{asset('assets/plugins/custom/formrepeater/formrepeater.bundle.js')}}"></script>
+    <script src="{{asset('assets/js/cms/audios/save-audio.js')}}"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/i18n/ar.min.js"></script>
+
+@endsection
