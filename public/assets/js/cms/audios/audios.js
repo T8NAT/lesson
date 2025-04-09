4bd234c77eb981f473ac31c658f5b8420d299b77
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-09 12:15:54 +0300

    Create audios.js

---
+++ public/assets/js/cms/audios/audios.js
@@ -0,0 +1,131 @@
+"use strict";
+
+let messages = {};
+
+var lang = $('html').attr('lang');
+
+const currentLanguage = document.documentElement.lang || "ar";
+
+function loadMessagesWithCache(lang) {
+    const cachedMessages = localStorage.getItem(`messages_${lang}`);
+    if (cachedMessages) {
+        messages = JSON.parse(cachedMessages);
+        return Promise.resolve();
+    } else {
+        return $.getJSON(`/assets/js/custom/apps/ecommerce/language/languages.json`, function (data) {
+            messages = data[lang] || data["en"];
+            localStorage.setItem(`messages_${lang}`, JSON.stringify(messages));
+        });
+    }
+}
+
+loadMessagesWithCache(currentLanguage).then(() => {
+    console.log("Messages loaded:", messages);
+});
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+
+
+var KTAppAudios = function () {
+    return {
+        init: function () {
+            const table = $("#kt_audio_table").DataTable({
+                processing: true,
+                serverSide: true,
+                language: {
+                    url: '/assets/js/custom/apps/ecommerce/language/ar.json'
+                },
+                ajax: {
+                    url: '/dashboard/audios/',
+                    type: "GET",
+                },
+                columns: [
+                    // {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                    // {data: 'id', name: 'id'},
+                    { data: "checkbox", orderable: false, searchable: false },
+                    {data: 'name', name: 'name'},
+                    {data: 'description', name: 'description'},
+                    {data: 'actions', name: 'actions', orderable: true, searchable: true},
+                ],
+                order: [[1, "asc"]]
+
+            });
+
+            // Delete functionality
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            table.on("click", '[data-kt-audio-filter="delete_row"]', function () {
+                const audioId = $(this).closest("div[data-audio-id]").data("audio-id");
+                const audioName = $(this).closest("div[data-audio-name]").data("audio-name");
+
+                Swal.fire({
+                    text: `${messages.Deleted} ${audioName}?`,
+                    icon: "warning",
+                    showCancelButton: true,
+                    buttonsStyling: false,
+                    confirmButtonText:messages.YesButtonText,
+                    cancelButtonText: messages.NoButtonText,
+                    customClass: {
+                        confirmButton: "btn fw-bold btn-danger",
+                        cancelButton: "btn fw-bold btn-active-light-primary"
+                    }
+                }).then((result) => {
+                    if (result.value) {
+                        Swal.fire({
+                            text: messages.Deleting + audioName + "...",
+                            icon: "info",
+                            showConfirmButton: false,
+                            allowOutsideClick: false
+                        });
+                        $.ajax({
+                            url:  '/dashboard/audios/' + audioId,
+                            type: "DELETE",
+                            success: function (response) {
+                                Swal.fire({
+                                    text: response.text,
+                                    icon: response.icon,
+                                    buttonsStyling: false,
+                                    confirmButtonText: messages.confirmButtonText,
+                                    customClass: { confirmButton: "btn fw-bold btn-primary" }
+                                }).then(() => table.ajax.reload());
+                            },
+                            error: function () {
+                                Swal.fire({
+                                    text: messages.genericError,
+                                    icon: "error",
+                                    buttonsStyling: false,
+                                    confirmButtonText: messages.confirmButtonText,
+                                    customClass: { confirmButton: "btn fw-bold btn-primary" }
+                                });
+                            }
+                        });
+                    }else if(result.dismiss === Swal.DismissReason.cancel) {
+                        Swal.fire({
+                            text: imageName + messages.NotDeleted,
+                            icon: "error",
+                            buttonsStyling: false,
+                            confirmButtonText: messages.confirmButtonText,
+                            customClass: { confirmButton: "btn fw-bold btn-primary" }
+                        });
+                    }
+                });
+            });
+        }
+    };
+}();
+
+$('#kt_audio_table').on('draw.dt', function () {
+    KTMenu.createInstances();
+});
+KTUtil.onDOMContentLoaded(function () {
+    KTAppAudios.init();
+});
