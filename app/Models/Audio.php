7bd5e85a45b6ac195af7404e42c2ed7429aeba16
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-09 12:15:31 +0300

    Create Audio.php

---
+++ app/Models/Audio.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Audio extends Model
+{
+    protected $fillable = [
+        'name',
+        'path',
+        'description',
+    ];
+
+    public function words(): HasMany
+    {
+        return $this->hasMany(Word::class);
+    }
+}
