34f1ae4a55f197ad3b77b7119df6aeae181d2ee6
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-12 15:27:30 +0300

    Update Game.php

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -18,7 +18,6 @@ class Game extends Model
         'category_id',
         'type_id',
         'status',
-        'images',
     ];
     public function categories(): BelongsToMany
     {
@@ -34,12 +33,9 @@ class Game extends Model
     {
         return $this->belongsTo(Type::class);
     }
-    public function images(): HasMany
+
+    public function levels(): BelongsToMany
     {
-        return $this->hasMany(Image::class);
-    }
-    public function levels(): hasMany
-    {
-        return $this->hasMany(Level::class)->orderBy('level_number');
+        return $this->belongsToMany(Level::class, 'game_level');
     }
 }
