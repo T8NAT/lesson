c1fd7fac82037b755f7fd619079fcee8a4e32b30
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-12 15:27:28 +0300

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -18,6 +19,10 @@ class Category extends Model
     public function games(): BelongsToMany
     {
         return $this->belongsToMany(Game::class, 'category_game', 'category_id', 'game_id');
+    }
+    public function levels(): hasMany
+    {
+        return $this->hasMany(Level::class);
     }
     public function words(){
         return $this->hasMany(Word::class);
