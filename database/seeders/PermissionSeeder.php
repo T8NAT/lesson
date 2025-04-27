245b4d88ddd941aef0fc280a2afc292b4fc0c713
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-04-27 10:09:25 +0300

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -24,9 +24,15 @@ class PermissionSeeder extends Seeder
                     'admin'      =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
                     'sponsor'    =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
                     'category'   =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
-//                    'user'       =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+//                  'user'       =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
                     'teacher'    =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
                     'student'    =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+                    'game'       =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+                    'word'       =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+                    'level'      =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+                    'image'      =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+                    'audio'      =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
+                    'type'      =>  ['can-add'=>'1','can-edit'=>'1','can-delete'=>'1','can-show'=>'1'],
                 ]
             ],
         ] ;
